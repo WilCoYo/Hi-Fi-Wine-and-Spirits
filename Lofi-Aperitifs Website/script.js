f447e2465726a42733687f502c36df2aa3f22c24
W. Cody Yonkin <WilCoYonkin@gmail.com>
2024-04-03 10:59:59 -0400

    lots of changes. Editted out icons on top nav. 86d location of nearby stores

--- Lofi-Aperitifs Website/script.js
+++ Lofi-Aperitifs Website/script.js
@@ -63,29 +63,17 @@ const scrollFunction = () => {
 
 }
 
-let autocomplete;
 
-function initAutocomplete() {
-  autocomplete = new google.maps.places.Autocomplete(
-    document.getElementById('autocomplete'),
-    {
-      types: ['address'],
-      componentRestrictions: {'country': ['US']},
-      fields: ['place_id', 'geometry', 'name']
-    });
 
-  autocomplete.addListener('place_changed', onPlaceChanged);
-}
 
-function onPlaceChanged(){
-  const place = autocomplete.getPlace();
 
-  if(!place.geometry){
-    document.getElementById('autocomplete').placeholder = 'Enter an address';
-  } else {
-    document.getElementById('details').innerHTML = place.name;
-  }
-}
+
+
+
+
+
+
+
 
 
 
