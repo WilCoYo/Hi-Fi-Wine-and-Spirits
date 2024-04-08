cfc1f92a93a771ddf8d8c9600149ac7517cc3047
W. Cody Yonkin <WilCoYonkin@gmail.com>
2024-04-08 12:05:28 -0400

    finished adding all desktop and mobile recipes on cocktails page

--- Lofi-Aperitifs Website/script.js
+++ Lofi-Aperitifs Website/script.js
@@ -147,6 +147,304 @@ const amaropinkRecipeMobile = () => {
 
 
 
+const highpointRecipe = () => {
+  if(document.getElementById('highpoint-recipe').style.display === "none"){
+    document.getElementById('highpoint-recipe').style.display = '';
+    document.getElementById('highpoint-caption').style.display = 'none';
+  } else {
+    document.getElementById('highpoint-recipe').style.display = 'none';
+    document.getElementById('highpoint-caption').style.display = '';
+  }
+
+}
+
+const highpointRecipeMobile = () => {
+  if(document.getElementById("mobile-highpoint-recipe").style.display === "none"){
+    document.getElementById("mobile-highpoint-recipe").style.display = '';
+    document.getElementById("highpoint-recipe-button").innerHTML = "HIDE RECIPE"
+  } else {
+    document.getElementById("mobile-highpoint-recipe").style.display = 'none';
+    document.getElementById("highpoint-recipe-button").innerHTML = "SHOW RECIPE"
+  }
+ 
+}
+
+
+
+
+const vtRecipe = () => {
+  if(document.getElementById('vt-recipe').style.display === "none"){
+    document.getElementById('vt-recipe').style.display = '';
+    document.getElementById('vt-caption').style.display = 'none';
+  } else {
+    document.getElementById('vt-recipe').style.display = 'none';
+    document.getElementById('vt-caption').style.display = '';
+  }
+
+}
+
+const vtRecipeMobile = () => {
+  if(document.getElementById("mobile-vt-recipe").style.display === "none"){
+    document.getElementById("mobile-vt-recipe").style.display = '';
+    document.getElementById("vt-recipe-button").innerHTML = "HIDE RECIPE"
+  } else {
+    document.getElementById("mobile-vt-recipe").style.display = 'none';
+    document.getElementById("vt-recipe-button").innerHTML = "SHOW RECIPE"
+  }
+ 
+}
+
+
+
+
+const pinkhighballRecipe = () => {
+  if(document.getElementById('pinkhighball-recipe').style.display === "none"){
+    document.getElementById('pinkhighball-recipe').style.display = '';
+    document.getElementById('pinkhighball-caption').style.display = 'none';
+  } else {
+    document.getElementById('pinkhighball-recipe').style.display = 'none';
+    document.getElementById('pinkhighball-caption').style.display = '';
+  }
+
+}
+
+const pinkhighballRecipeMobile = () => {
+  if(document.getElementById("mobile-pinkhighball-recipe").style.display === "none"){
+    document.getElementById("mobile-pinkhighball-recipe").style.display = '';
+    document.getElementById("pinkhighball-recipe-button").innerHTML = "HIDE RECIPE"
+  } else {
+    document.getElementById("mobile-pinkhighball-recipe").style.display = 'none';
+    document.getElementById("pinkhighball-recipe-button").innerHTML = "SHOW RECIPE"
+  }
+ 
+}
+
+
+
+
+const lotusRecipe = () => {
+  if(document.getElementById('lotus-recipe').style.display === "none"){
+    document.getElementById('lotus-recipe').style.display = '';
+    document.getElementById('lotus-caption').style.display = 'none';
+  } else {
+    document.getElementById('lotus-recipe').style.display = 'none';
+    document.getElementById('lotus-caption').style.display = '';
+  }
+
+}
+
+const lotusRecipeMobile = () => {
+  if(document.getElementById("mobile-lotus-recipe").style.display === "none"){
+    document.getElementById("mobile-lotus-recipe").style.display = '';
+    document.getElementById("lotus-recipe-button").innerHTML = "HIDE RECIPE"
+  } else {
+    document.getElementById("mobile-lotus-recipe").style.display = 'none';
+    document.getElementById("lotus-recipe-button").innerHTML = "SHOW RECIPE"
+  }
+ 
+}
+
+
+
+const pinkbeerRecipe = () => {
+  if(document.getElementById('pinkbeer-recipe').style.display === "none"){
+    document.getElementById('pinkbeer-recipe').style.display = '';
+    document.getElementById('pinkbeer-caption').style.display = 'none';
+  } else {
+    document.getElementById('pinkbeer-recipe').style.display = 'none';
+    document.getElementById('pinkbeer-caption').style.display = '';
+  }
+
+}
+
+const pinkbeerRecipeMobile = () => {
+  if(document.getElementById("mobile-pinkbeer-recipe").style.display === "none"){
+    document.getElementById("mobile-pinkbeer-recipe").style.display = '';
+    document.getElementById("pinkbeer-recipe-button").innerHTML = "HIDE RECIPE"
+  } else {
+    document.getElementById("mobile-pinkbeer-recipe").style.display = 'none';
+    document.getElementById("pinkbeer-recipe-button").innerHTML = "SHOW RECIPE"
+  }
+ 
+}
+
+
+
+
+const beesmokerRecipe = () => {
+  if(document.getElementById('beesmoker-recipe').style.display === "none"){
+    document.getElementById('beesmoker-recipe').style.display = '';
+    document.getElementById('beesmoker-caption').style.display = 'none';
+  } else {
+    document.getElementById('beesmoker-recipe').style.display = 'none';
+    document.getElementById('beesmoker-caption').style.display = '';
+  }
+
+}
+
+const beesmokerRecipeMobile = () => {
+  if(document.getElementById("mobile-beesmoker-recipe").style.display === "none"){
+    document.getElementById("mobile-beesmoker-recipe").style.display = '';
+    document.getElementById("beesmoker-recipe-button").innerHTML = "HIDE RECIPE"
+  } else {
+    document.getElementById("mobile-beesmoker-recipe").style.display = 'none';
+    document.getElementById("beesmoker-recipe-button").innerHTML = "SHOW RECIPE"
+  }
+ 
+}
+
+
+
+
+const whiteElephantRecipe = () => {
+  if(document.getElementById('whiteElephant-recipe').style.display === "none"){
+    document.getElementById('whiteElephant-recipe').style.display = '';
+    document.getElementById('whiteElephant-caption').style.display = 'none';
+  } else {
+    document.getElementById('whiteElephant-recipe').style.display = 'none';
+    document.getElementById('whiteElephant-caption').style.display = '';
+  }
+
+}
+
+const whiteElephantRecipeMobile = () => {
+  if(document.getElementById("mobile-whiteElephant-recipe").style.display === "none"){
+    document.getElementById("mobile-whiteElephant-recipe").style.display = '';
+    document.getElementById("whiteElephant-recipe-button").innerHTML = "HIDE RECIPE"
+  } else {
+    document.getElementById("mobile-whiteElephant-recipe").style.display = 'none';
+    document.getElementById("whiteElephant-recipe-button").innerHTML = "SHOW RECIPE"
+  }
+ 
+}
+
+
+
+
+const extradryRecipe = () => {
+  if(document.getElementById('extradry-recipe').style.display === "none"){
+    document.getElementById('extradry-recipe').style.display = '';
+    document.getElementById('extradry-caption').style.display = 'none';
+  } else {
+    document.getElementById('extradry-recipe').style.display = 'none';
+    document.getElementById('extradry-caption').style.display = '';
+  }
+
+}
+
+const extradryRecipeMobile = () => {
+  if(document.getElementById("mobile-extradry-recipe").style.display === "none"){
+    document.getElementById("mobile-extradry-recipe").style.display = '';
+    document.getElementById("extradry-recipe-button").innerHTML = "HIDE RECIPE"
+  } else {
+    document.getElementById("mobile-extradry-recipe").style.display = 'none';
+    document.getElementById("extradry-recipe-button").innerHTML = "SHOW RECIPE"
+  }
+ 
+}
+
+
+
+
+const saturnreturnsRecipe = () => {
+  if(document.getElementById('saturnreturns-recipe').style.display === "none"){
+    document.getElementById('saturnreturns-recipe').style.display = '';
+    document.getElementById('saturnreturns-caption').style.display = 'none';
+  } else {
+    document.getElementById('saturnreturns-recipe').style.display = 'none';
+    document.getElementById('saturnreturns-caption').style.display = '';
+  }
+
+}
+
+const saturnreturnsRecipeMobile = () => {
+  if(document.getElementById("mobile-saturnreturns-recipe").style.display === "none"){
+    document.getElementById("mobile-saturnreturns-recipe").style.display = '';
+    document.getElementById("saturnreturns-recipe-button").innerHTML = "HIDE RECIPE"
+  } else {
+    document.getElementById("mobile-saturnreturns-recipe").style.display = 'none';
+    document.getElementById("saturnreturns-recipe-button").innerHTML = "SHOW RECIPE"
+  }
+ 
+}
+
+
+
+
+const americanoRecipe = () => {
+  if(document.getElementById('americano-recipe').style.display === "none"){
+    document.getElementById('americano-recipe').style.display = '';
+    document.getElementById('americano-caption').style.display = 'none';
+  } else {
+    document.getElementById('americano-recipe').style.display = 'none';
+    document.getElementById('americano-caption').style.display = '';
+  }
+
+}
+
+const americanoRecipeMobile = () => {
+  if(document.getElementById("mobile-americano-recipe").style.display === "none"){
+    document.getElementById("mobile-americano-recipe").style.display = '';
+    document.getElementById("americano-recipe-button").innerHTML = "HIDE RECIPE"
+  } else {
+    document.getElementById("mobile-americano-recipe").style.display = 'none';
+    document.getElementById("americano-recipe-button").innerHTML = "SHOW RECIPE"
+  }
+ 
+}
+
+
+
+
+const lofispritzRecipe = () => {
+  if(document.getElementById('lofispritz-recipe').style.display === "none"){
+    document.getElementById('lofispritz-recipe').style.display = '';
+    document.getElementById('lofispritz-caption').style.display = 'none';
+  } else {
+    document.getElementById('lofispritz-recipe').style.display = 'none';
+    document.getElementById('lofispritz-caption').style.display = '';
+  }
+
+}
+
+const lofispritzRecipeMobile = () => {
+  if(document.getElementById("mobile-lofispritz-recipe").style.display === "none"){
+    document.getElementById("mobile-lofispritz-recipe").style.display = '';
+    document.getElementById("lofispritz-recipe-button").innerHTML = "HIDE RECIPE"
+  } else {
+    document.getElementById("mobile-lofispritz-recipe").style.display = 'none';
+    document.getElementById("lofispritz-recipe-button").innerHTML = "SHOW RECIPE"
+  }
+ 
+}
+
+
+
+
+const pinkpalomaRecipe = () => {
+  if(document.getElementById('pinkpaloma-recipe').style.display === "none"){
+    document.getElementById('pinkpaloma-recipe').style.display = '';
+    document.getElementById('pinkpaloma-caption').style.display = 'none';
+  } else {
+    document.getElementById('pinkpaloma-recipe').style.display = 'none';
+    document.getElementById('pinkpaloma-caption').style.display = '';
+  }
+
+}
+
+const pinkpalomaRecipeMobile = () => {
+  if(document.getElementById("mobile-pinkpaloma-recipe").style.display === "none"){
+    document.getElementById("mobile-pinkpaloma-recipe").style.display = '';
+    document.getElementById("pinkpaloma-recipe-button").innerHTML = "HIDE RECIPE"
+  } else {
+    document.getElementById("mobile-pinkpaloma-recipe").style.display = 'none';
+    document.getElementById("pinkpaloma-recipe-button").innerHTML = "SHOW RECIPE"
+  }
+ 
+}
+
+
+
 
 
 
